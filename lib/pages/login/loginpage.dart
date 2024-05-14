5f906b7c95801a9835c923e9a389b4b297d37317
brijeshchhatrala01 <dev.brijesh.chhatrala@gmail.com>
2024-05-14 22:57:30 +0530

    initial  commit

--- lib/pages/login/loginpage.dart
+++ lib/pages/login/loginpage.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: unused_local_variable, use_build_context_synchronously
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:messaging_app/pages/components/custom_button.dart';
